683b4979d82aa10a61955c425d0a4ea7fa8bbb54
amitpatelgit <amitpatel.git@gmail.com>
2022-09-12 13:48:47 +0530

    uploading files

---
+++ src/Counter.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() {
+        return (
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button
+                    onClick={() => this.props.onIncrement(this.props.counter)}
+                    className="btn btn-secondary btn-"
+                >
+                    Increment
+                </button>
+                <button
+                    onClick={() => this.props.onDelete(this.props.counter.id)}
+                    className="btn btn-danger btn- m-2"
+                >
+                    Delete
+                </button>
+            </div>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = this.props.counter.value === 0 ? "badge p-2 m-2 bg-warning text-dark" : "badge p-1 m-3 bg-primary text-white";
+        return classes;
+    }
+
+    formatCount() {
+        const { value } = this.props.counter;
+        return value === 0 ? "Zero" : value;
+    }
+}
+
+export default Counter;
